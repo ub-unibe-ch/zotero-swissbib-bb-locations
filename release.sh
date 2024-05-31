89307179fd51a4e5e2cfc73f47d633901c45d223
Denis Maier <denis.maier@unibe.ch>
2024-05-31 22:40:16 +0200

    Version 12

--- release.sh
+++ release.sh
@@ -22,5 +22,6 @@ perl -pi -e "s/em:version=\"[^\"]*/em:version=\"$version/;" src/install.rdf
 
 git add src/install.rdf #src/manifest.json
 git commit -m "Version ${version}" 1>&2
+git push
 git tag -a -m "Version ${version}" "${version}"
-git push --tags origin
+git push origin "${version}"
