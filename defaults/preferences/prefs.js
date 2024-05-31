29004a57ab1d081d3e666119bcf3d22f28368782
Denis Maier <denis.maier@unibe.ch>
2024-05-31 17:08:43 +0200

    add workflow

--- defaults/preferences/prefs.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.swisscoveryubbernlocations.apikey", "");
-pref("extensions.swisscoveryubbernlocations.sruurl", "https://slsp-ube.alma.exlibrisgroup.com/view/sru/41SLSP_UBE?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.isbn=");
-pref("extensions.swisscoveryubbernlocations.targetField", "abstractNote");
