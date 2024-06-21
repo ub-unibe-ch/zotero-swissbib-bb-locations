bc26d929a92d15e9670be8288df3653a6f38833b
Denis Maier <denis.maier@unibe.ch>
2024-06-21 11:32:45 +0200

    refactor for z7

---
+++ src/prefs.js
@@ -0,0 +1,3 @@
+pref("extensions.sul.apikey", "");
+pref("extensions.sul.sruurl", "https://slsp-ube.alma.exlibrisgroup.com/view/sru/41SLSP_UBE?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=alma.isbn=");
+pref("extensions.sul.targetField", "abstractNote");
